a988e5b1c22b724ce7afb8c86dbe4445fd8ed8af
CALEB JESUSEGUN <jesuseguncaleb@gmail.com>
2023-02-08 11:06:04 +0100

    chore: Revert codebase to starter project files

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,14 @@
-import 'dart:io';
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:resumeapp/provider/theme_provider.dart';
 import 'package:resumeapp/ui/views/resume.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
-      ],
-      child: DevicePreview(
-          enabled: false,
-          builder: (context) => const MyApp())));
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (_) => ThemeProvider()),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -24,31 +17,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final themeProvider = context.watch<ThemeProvider>();
-    final String deviceLocale= Platform.localeName.split('-')[0];
     return ScreenUtilInit(
-          designSize: const Size(375, 812),
-          minTextAdapt: true,
-          builder: (context, child) {
-            return MaterialApp(
-              localizationsDelegates: AppLocalizations.localizationsDelegates,
-              supportedLocales: AppLocalizations.supportedLocales,
-              localeResolutionCallback: (Locale? locale, Iterable<Locale> supportedLocales) {
-                for (final supportedLocale in supportedLocales) {
-                  if (deviceLocale == supportedLocale.languageCode) {
-                    return supportedLocale;
-                  }
-                }
-                return supportedLocales.first;
-              },
-              builder: DevicePreview.appBuilder,
-              themeMode: themeProvider.themeMode,
-              useInheritedMediaQuery: true,
-              theme: MyThemes.lightTheme,
-              darkTheme: MyThemes.darkTheme,
-              debugShowCheckedModeBanner: false,
-              home: const ResumeView(),
-            );
-          },
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return MaterialApp(
+          themeMode: themeProvider.themeMode,
+          useInheritedMediaQuery: true,
+          theme: MyThemes.lightTheme,
+          darkTheme: MyThemes.darkTheme,
+          debugShowCheckedModeBanner: false,
+          home: const ResumeView(),
         );
+      },
+    );
   }
 }
